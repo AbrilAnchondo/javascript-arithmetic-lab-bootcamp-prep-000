f63b93e2c9a4b11f56a3e571567efb3179f59af9
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-07-21 01:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,21 +22,14 @@ function dec(n) {
   return n -= 1;
 }
 
-/*function makeInt(n) {
-  if(isNaN(n)){return NaN}
-  
-  
- parseInt ('n', 10);
- return n;
-  }*/
 
 function makeInt (n) {
   return parseInt (n, 10);
 }
 
-/*function preserveDecimal(n) {
+function preserveDecimal(n) {
  return parseFloat (n);
-}*/
+}
 
     
   
